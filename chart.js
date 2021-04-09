7c989a1dd59bb361c5d3c9e4c4ff068370062257
Nunny R <nunnyreyes@gmail.com>
2021-04-08 20:01:58 -0400

    current

---
+++ chart.js
@@ -0,0 +1,16 @@
+import {scrapers} from scrapers.js
+
+
+//<canvas id="myChart" width="400" height="400"></canvas>
+// const config = {
+//     type: 'scatter',
+//     data: data,
+//     options: {
+//       scales: {
+//         x: {
+//           type: 'linear',
+//           position: 'bottom'
+//         }
+//       }
+//     }
+//   };
